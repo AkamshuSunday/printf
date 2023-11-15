df9f52f3f42add464247c3bf29de59364f4d6f21
“AkamshuSunday” <akamshu.sunday6@gmail.com>
2023-11-14 22:44:05 -0800

    collaborated project

--- main.h
+++ main.h
@@ -1,8 +1,13 @@
-#ifndef MAIN_H #define MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
 int _printf(const char *format, ...);
 int _putchar(char c);
 int _puts(char *str);
+int _print_number(int n);
+int _print_unsigned(unsigned int n);
+int _print_octal(unsigned int n);
+int _print_hex(unsigned int n, int uppercase);
 
 #endif /* MAIN_H */
 
