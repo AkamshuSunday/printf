77b0cd61a70b6ff0c9f009c4fee8f2449c0673c3
“AkamshuSunday” <akamshu.sunday6@gmail.com>
2023-11-15 05:35:51 -0800

    Collaborated project

--- get_size.c
+++ get_size.c
@@ -12,14 +12,14 @@ int get_size(const char *format, int *i)
     int curr_i = *i + 1;
     int size = 0;
 
-    // Check if 'l' denotes a long size specifier.
+    /* Check if 'l' denotes a long size specifier.*/
     if (format[curr_i] == 'l')
         size = S_LONG;
-    // Check if 'h' denotes a short size specifier.
+    /* Check if 'h' denotes a short size specifier.*/
     else if (format[curr_i] == 'h')
         size = S_SHORT;
 
-    // If no size specifier is found, update the index accordingly.
+    /* If no size specifier is found, update the index accordingly.*/
     if (size == 0)
         *i = curr_i - 1;
     else
